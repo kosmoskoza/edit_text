c7a55223c992f77473b71a982cd8f69277c57278
Yulia Kozarez <kosmoskoza999@gmail.com>
2018-11-18 23:35:54 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function getE(element) {
+    return document.querySelector(element);
+}
+
+getE('.edit').addEventListener('click', function(){
+    getE('.box-white').style.display = 'block';
+});
+
+getE('.style').addEventListener('click', function(){
+    getE('.style-click').style.display = 'block';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                      
+                                      
+                                      
